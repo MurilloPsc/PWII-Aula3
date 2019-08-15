350e03dcec597e94803570d55f8a44addf8aa628
MurilloPsc <48369082+MurilloPsc@users.noreply.github.com>
2019-08-14 22:17:06 -0300

    Add files via upload

--- revisao.js
+++ revisao.js
@@ -1,13 +1,9 @@
-var nota1 = 10;
-var nota2 = 9;
-var nota3 = 5;
-var total = 0;
-total = nota1 + nota2 + nota3;
-total = total / 3;
-if (function (total) { return 7; }) {
-    console.log("Aprovado");
-}
-else {
-    console.log("Reprovado");
-    console.log("A média é " + total);
-}
+var n1 = 10;
+var n2 = 9;
+//total = nota1 + nota2 + nota3
+//total = total/3
+var fMedia = function (n1, n2, n3) {
+    if (n3 === void 0) { n3 = 8; }
+    return (n1 + n2 + n3) / 3;
+};
+console.log("A m\u00E9dia \u00E9 : " + fMedia(n1, n2));
--- revisao.ts
+++ revisao.ts
@@ -1,7 +1,11 @@
-let nota1 : number = 10
-let nota2 : number = 9
-let nota3 : number = 5
-let total : number = 0
-total = nota1 + nota2 + nota3
-total = total/3
-console.log("A média é " + total)
+let n1 : number = 10
+let n2 : number = 9
+
+//total = nota1 + nota2 + nota3
+//total = total/3
+
+let fMedia = (n1:number, n2:number, n3:number=8)=>(n1+n2+n3)/3
+
+
+
+console.log(`A média é : ${fMedia(n1,n2)}`)
